623465ba3d21cb6cd0fb175478e38581dd07a7da
techno-tanoC <techno.tanoc@gmail.com>
2014-11-11 19:30:34 +0900

    add Mylist

--- lib/nico_util/mylist.rb
+++ lib/nico_util/mylist.rb
@@ -14,13 +14,29 @@ module NicoUtil
 
     URL = /(http:\/\/www\.nicovideo\.jp\/mylist\/)?(\d+)(\?rss=2\.0)?/
     def initialize(group_id)
-      builder = method(:build_url) >> method(:open) >> REXML::Document._.new >> (get_elements & "/rss/channel") >> :first
-#      doc =
+      builder = method(:build_url) >> method(:open) >> :read >> REXML::Document._.new >> (:get_elements & "rss/channel") >> :first
+
+      doc =
         if URL =~ group_id
           builder < $2
         else
           raise "not mylist url or unexist mylist"
         end
+
+      [
+        :title, :link, :description, :pubDate, :lastBuildDate
+      ].each do |sym|
+        attr_setter(sym, :to_s >> doc._.text < sym)
+      end
+      attr_setter(:creator, doc._.text < "dc:creator")
+
+      attr_setter(:items) do
+        doc.get_elements("item").map do |item|
+          [:title, :link, :pubDate, :description].map {|sym|
+            [sym, :to_s >> item._.text < sym]
+          }.to_h
+        end
+      end
     end
 
     def to_h
--- spec/nico_util/mylist_spec.rb
+++ spec/nico_util/mylist_spec.rb
@@ -1,12 +1,17 @@
 # coding: utf-8
 
 require 'spec_helper'
+require 'pp'
 
 ok_xml = open("spec/files/ok.xml").read
 
 describe 'NicoUtil' do
   describe 'Mylist' do
     subject { NicoUtil::Mylist.new(ok_xml) }
+
+    it 'test' do
+      pp NicoUtil::Mylist.new("45604545")
+    end
 
     describe '#build_url' do
     end
